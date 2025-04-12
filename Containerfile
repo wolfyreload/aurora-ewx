f5437bf682ec233743529558c087902ea5a843dc
wolfyreload <open.chords.app@gmail.com>
2025-04-12 19:07:08 +0200

    Add teams for Linux

--- Containerfile
+++ Containerfile
@@ -18,6 +18,7 @@ FROM ghcr.io/ublue-os/aurora-dx:stable-daily
 ## make modifications desired in your image and install packages by modifying the build.sh script
 ## the following RUN directive does all the things required to run "build.sh" as recommended.
 
+COPY system_files /
 RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
     --mount=type=cache,dst=/var/cache \
     --mount=type=cache,dst=/var/log \
--- build_files/build.sh
+++ build_files/build.sh
@@ -17,3 +17,11 @@ chmod +x /usr/bin/dotnet-install
 # Add nvm installer 
 wget https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh -O /usr/bin/nvm-install
 chmod +x /usr/bin/nvm-install
+
+# Add teams for Linux
+wget https://github.com/IsmaelMartinez/teams-for-linux/releases/download/v1.12.8/teams-for-linux-1.12.8.AppImage -O /usr/bin/teams_for_linux.appimage
+chmod +x /usr/bin/teams_for_linux.appimage
+
+# Add openfortivpn-webview
+wget https://github.com/gm-vm/openfortivpn-webview/releases/download/v1.2.3-electron/openfortivpn-webview-1.2.3-x86_64.AppImage -O /usr/bin/openfortivpn-webview.appimage
+chmod +x /usr/bin/openfortivpn-webview.appimage
