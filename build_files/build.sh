312c964ff7cddca6e0ea59345d4d93b96b30cd10
wolfyreload <open.chords.app@gmail.com>
2025-05-23 12:22:43 +0200

    chore: cleanup merge conflict handling in build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
 
-# ### Install packages
-
-# # Packages can be installed from any enabled yum repo on the image.
-# # RPMfusion repos are available by default in ublue main images
-# # List of rpmfusion packages can be found here:
-# # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# # this installs a package from fedora repos
-# dnf5 install -y tmux 
-
-# # Use a COPR Example:
-# #
-# # dnf5 -y copr enable ublue-os/staging
-# # dnf5 -y install package
-# # Disable COPRs so they don't end up enabled on the final image:
-# # dnf5 -y copr disable ublue-os/staging
-
-# #### Example for enabling a System Unit File
-
-# systemctl enable podman.socket
-
 set -ouex pipefail
 
 # this installs a package from fedora repos
@@ -46,3 +25,30 @@ chmod +x /usr/bin/teams_for_linux.appimage
 # Add openfortivpn-webview
 wget https://github.com/gm-vm/openfortivpn-webview/releases/download/v1.2.3-electron/openfortivpn-webview-1.2.3-x86_64.AppImage -O /usr/bin/openfortivpn-webview.appimage
 chmod +x /usr/bin/openfortivpn-webview.appimage
+
+<<'###BLOCK-COMMENT'
+#!/bin/bash
+
+set -ouex pipefail
+
+### Install packages
+
+# Packages can be installed from any enabled yum repo on the image.
+# RPMfusion repos are available by default in ublue main images
+# List of rpmfusion packages can be found here:
+# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+
+# this installs a package from fedora repos
+dnf5 install -y tmux 
+
+# Use a COPR Example:
+#
+# dnf5 -y copr enable ublue-os/staging
+# dnf5 -y install package
+# Disable COPRs so they don't end up enabled on the final image
+# dnf5 -y copr disable ublue-os/staging
+
+#### Example for enabling a System Unit File
+
+systemctl enable podman.socket
+###BLOCK-COMMENT
