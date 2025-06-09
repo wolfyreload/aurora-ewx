a28f9c3ef534554c3b059c34a10a31fe877ececb
wolfyreload <5320719+wolfyreload@users.noreply.github.com>
2025-06-09 17:48:23 +0200

    chore: add intune-portal

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,7 +5,8 @@ set -ouex pipefail
 # this installs a package from fedora repos
 dnf5 install -y openfortivpn \
         qdirstat \
-        earlyoom
+        earlyoom \
+        intune-portal
 
 # Downlaod and install Azure data studio
 wget https://go.microsoft.com/fwlink/?linkid=2301926 -O /tmp/azure-data-studio.rpm
