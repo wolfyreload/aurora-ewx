68e20210b7c0ab120b409a3e7cf3548004cd3863
wolfyreload <5320719+wolfyreload@users.noreply.github.com>
2025-07-04 12:38:54 +0200

    chore: swap earlyoom with nohang

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,7 +4,7 @@ set -ouex pipefail
 
 # this installs a package from fedora repos
 dnf5 install -y openfortivpn \
-        earlyoom 
+        nohang 
 
 # Downlaod and install Azure data studio
 wget https://go.microsoft.com/fwlink/?linkid=2301926 -O /tmp/azure-data-studio.rpm
@@ -33,7 +33,7 @@ wget \
 chmod +x /usr/bin/openfortivpn-webview.appimage
 
 # Enable earlyoom
-systemctl enable earlyoom.service
+systemctl enable nohang-desktop.service
 
 <<'###BLOCK-COMMENT'
 #!/bin/bash
