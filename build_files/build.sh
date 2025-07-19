0ee95fd95ad4ec2e7e55a5470e607fd0b8801bf0
wolfyreload <5320719+wolfyreload@users.noreply.github.com>
2025-07-19 17:35:12 +0200

    chore: fix openfortivpn-webview download

--- build_files/build.sh
+++ build_files/build.sh
@@ -27,9 +27,8 @@ chmod +x /usr/bin/teams_for_linux.appimage
 
 # Add openfortivpn-webview
 wget \
-     $(curl -s https://api.github.com/repos/gm-vm/openfortivpn-webview/releases/latest | \
-     jq -r ".assets[] | select(.name | test(\".*x86_64[.]AppImage\")) | .browser_download_url") \
-     -O /usr/bin/openfortivpn-webview.appimage
+    https://github.com/gm-vm/openfortivpn-webview/releases/download/v1.2.3-electron/openfortivpn-webview-1.2.3-x86_64.AppImage \
+    -O /usr/bin/openfortivpn-webview.appimage
 chmod +x /usr/bin/openfortivpn-webview.appimage
 
 # Enable earlyoom
