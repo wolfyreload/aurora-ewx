3a5b992355026829c23ccf4ad6d488439880842b
wolfyreload <5320719+wolfyreload@users.noreply.github.com>
2026-07-19 19:00:28 +0200

    chore: remove earlyoom

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,15 +4,11 @@ set -ouex pipefail
 
 # this installs a package from fedora repos
 dnf5 install -y openfortivpn \
-        earlyoom \
         libgdiplus
 
 # Downlaod and install Azure data studio
 wget https://go.microsoft.com/fwlink/?linkid=2324527 -O /tmp/azure-data-studio.rpm
 dnf5 install -y /tmp/azure-data-studio.rpm
-
-# Enable earlyoom
-systemctl enable earlyoom.service
 
 <<'###BLOCK-COMMENT'
 #!/bin/bash
