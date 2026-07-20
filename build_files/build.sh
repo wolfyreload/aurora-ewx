d1e85dd461f0f766d583773cb1dffd620d063e23
wolfyreload <5320719+wolfyreload@users.noreply.github.com>
2026-07-20 06:36:04 +0200

    chore: add vscode support

--- build_files/build.sh
+++ build_files/build.sh
@@ -18,6 +18,18 @@ dnf5 -y install --enablerepo=docker-ce-stable \
 
 systemctl enable docker.socket
 
+# VS Code from Microsoft's official repo (same approach as Aurora DX)
+tee /etc/yum.repos.d/vscode.repo <<'EOF'
+[code]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+EOF
+sed -i "s/enabled=.*/enabled=0/g" /etc/yum.repos.d/vscode.repo
+dnf5 -y install --enablerepo=code code
+
 <<'###BLOCK-COMMENT'
 #!/bin/bash
 
